b7f2f68a0534ddf8da2ddab6e1fb9dd9af448fa2
tmtowtdi <tmtowtdi@gmail.com>
2015-04-23 14:48:25 -0400

    Version 2.10

--- lib/LacunaWaX.pm
+++ lib/LacunaWaX.pm
@@ -12,8 +12,8 @@ package LacunaWaX {
     use Wx::Event qw(EVT_MOVE EVT_CLOSE);
 
     use base 'Wx::App';
-    $Wx::App::VERSION   = "2.08.03";
-    our $VERSION        = '2.08.03';
+    $Wx::App::VERSION   = "2.10";
+    our $VERSION        = '2.10';
 
     use LacunaWaX::Preload::Perlapp;
     use LacunaWaX::MainFrame;
