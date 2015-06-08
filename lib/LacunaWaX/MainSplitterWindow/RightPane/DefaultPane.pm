935c9cf12a4f11d9e7a6d6a487eb068251a92a31
tmtowtdi <tmtowtdi@gmail.com>
2015-06-08 14:29:26 -0400

    Modified default screen text

--- lib/LacunaWaX/MainSplitterWindow/RightPane/DefaultPane.pm
+++ lib/LacunaWaX/MainSplitterWindow/RightPane/DefaultPane.pm
@@ -68,13 +68,9 @@ package LacunaWaX::MainSplitterWindow::RightPane::DefaultPane {
     sub _build_text {#{{{
         my $self = shift;
 
-        my $txt = "Now that you've logged in, be sure to check the Preferences window again.
+        my $txt = "Now that you've logged in, be sure to check the Preferences window again.  You entered your empire name and password there, but now that you're logged in, there are new options there that weren't available before.
 
-You had to check it to enter your empire name and password, but now that you're logged in, there are new options there that weren't available before logging in.
-
-If any of your Space Stations in the tree on the left are displaying child leaves that would only apply to Planets, not stations (eg 'Glyphs', 'Spies'), just double-click on the Station's name.  This will display a summary screen and update the tree to display the correct leaves.
-
-The main LacunaWaX program you're looking at right now provides some tools, and also allows you to set up preferences for exactly what you want the scheduled tasks (like Autovote) to do.
+The main LacunaWaX program you're looking at right now provides some tools, and also allows you to set up preferences for exactly what you want the scheduled tasks (like 'Auto Push Glyphs Home') to do.
 
 But you must set those scheduled tasks up separately!
 
